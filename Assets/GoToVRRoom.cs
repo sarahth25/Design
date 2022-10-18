5089faeb4bb12bc63ddc14065c75efc784273665
reembaset <reembaset6@gmail.com>
2022-10-18 10:43:11 +0300

    Initial commit

---
+++ Assets/GoToVRRoom.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToVRRoom : MonoBehaviour
+{
+    public void StartBtn()
+    {
+
+        SceneManager.LoadScene("VR room");
+
+    }
+}
